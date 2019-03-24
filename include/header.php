a94c80ec8fd38e3e5911c7e41b313a0964f68219
Eric <darmoneric@outlook.fr>
2019-03-24 22:21:40 +0100

    Ajout du fichier header dans include + préparation du header

---
+++ include/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            <?php
+                echo $title;
+            ?>
+        </title>
+        <link rel="stylesheet" type="text/css" href="styles/header_style.css">
+    </head>
+    <body>
+        <header>
+            <div class="brand">
+                <h1>
+                    <img src="images/logo_GL.png">
+                    <span>INTERNATIONAL AGENCY <strong class="red">FOR LIVE SHOWS</strong></span>
+                </h1>
+            </div>
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
