11cbd3b5058315b15fd7dd7984a4545ab4086c79
Eric <darmoneric@outlook.fr>
2019-05-02 11:10:05 +0200

    Finalisation de la page 404

--- page404.php
+++ page404.php
@@ -20,14 +20,17 @@
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     </head>
     <body>
-        <?php
-            $position = "relative";
-            include("include/header.php");
-        ?>
-        <h2>Page not found</h2>
-        <p>The page you requested was not found.</p>
-        <p>The link you used may be broken or you may have entered the URL incorrectly.</p>
-        
-        <button type="button" class="btn btn-outline-danger"><a href="index.php">BACK HOMEPAGE</a></button> 
+        <div id="main">
+            <?php
+                $position = "relative";
+                include("include/header.php");
+            ?>
+            <h2>Page not found</h2>
+            <p>The page you requested was not found.</p>
+            <p>The link you used may be broken or you may have entered the URL incorrectly.</p>
+
+            <a class="btn btn-outline-danger"href="index.php" id="btn-home">BACK HOMEPAGE</a>
+        </div>
+        <?php include("include/footer.php");?>
     </body>
 </html>
